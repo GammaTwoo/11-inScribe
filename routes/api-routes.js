3ec3e9e2659ffb6abecab276d67b55195d9d771b
GammaTwoo <gavin.frazier63@gmail.com>
2023-12-06 02:07:25 -0500

    post request created

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -9,6 +9,14 @@ router.get('/api/notes', async (req, res) => {
 
 router.post('/api/notes', (req, res) => {
     console.log('Post request received')
+    const data = JSON.parse(fs.readFileSync('db/db.json', 'utf8'))
+    const newData = {
+        title: req.body.title,
+        text: req.body.text
+    }
+    data.push(newData)
+    fs.writeFileSync('db/db.json', data)
+    res.json(data)
 })
 
 router.delete('/api/notes', (req, res) => {
