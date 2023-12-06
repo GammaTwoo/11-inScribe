81655b6d7506b1ca160f502e8ee653552e03188a
GammaTwoo <gavin.frazier63@gmail.com>
2023-12-06 00:05:16 -0500

    server actually functions, basic routes created

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const PORT = process.env.PORT || 3001
 const app = express()
 
 app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 app.use(express.static("public"))
 app.use(html_routes)
-app.use(api_routes)
+// app.use(api_routes)
 
 app.listen(PORT, () => {
     console.log(`server running on http://localhost:${PORT}`)
