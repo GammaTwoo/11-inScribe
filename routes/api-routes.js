f31a360916f3a0019a3702f237c3ee5a491dd003
GammaTwoo <gavin.frazier63@gmail.com>
2023-12-07 19:32:55 -0500

    i fucking hate software engineering

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,16 +1,13 @@
 const router = require('express').Router()
-// to make unique ids for each note
 const { v4: uuidv4 } = require('uuid')
 const fs = require ('fs')
 
-// route for get request. runs on page open, waits for data from the server to send the response
 router.get('/api/notes', async (req, res) => {
     console.log('Get request received')
     const data = await JSON.parse(fs.readFileSync('db/db.json', 'utf8'))
     res.json(data)
 })
 
-// route for post request. creates a note and adds it to the db
 router.post('/api/notes', (req, res) => {
     console.log('Post request received')
     const data = JSON.parse(fs.readFileSync('db/db.json', 'utf8'))
@@ -24,7 +21,6 @@ router.post('/api/notes', (req, res) => {
     res.json(data)
 })
 
-// delete request route. takes the db and creates a new object with the triggering note taken out, then writes the new data to the db 
 router.delete('/api/notes/:id', (req, res) => {
     console.log('Delete request received')
     console.log(req.params.id)
@@ -37,5 +33,4 @@ router.delete('/api/notes/:id', (req, res) => {
     res.json('Note Deleted')
 })
 
-// export the routes so server.js can use it
 module.exports = router
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,16 +1,12 @@
 const router = require('express').Router()
 const path = require('path')
 
-// any path entered requests the same index file. accept /notes    ||
-router.get('*', (req, res) => {                                 // ||
-    res.sendFile(path.join(__dirname, '../public/index.html'))  // ||
-})//            __________________________________________________ ||
-//            / ____________________________________________________/
-//            ||      
-// route for  \/    the notes request. only exception to previous route           
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'))
 })
 
-// export the routes so server.js can use
 module.exports = router
