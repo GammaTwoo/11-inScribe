1c61f6d556ce404cfe157c29d63ddd690d4e6d16
GammaTwoo <gavin.frazier63@gmail.com>
2023-12-07 19:41:51 -0500

    had to rearrange some lines so things loaded in the right order

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express()
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(express.static("public"))
-app.use(html_routes)
 app.use(api_routes)
+app.use(html_routes)
 
 app.listen(PORT, () => {
     console.log(`server running on http://localhost:${PORT}`)
